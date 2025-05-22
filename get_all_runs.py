9afb97ae1eb6b5744d53648a85c76080dc48d6eb
ding xuan <dhu003@e.ntu.edu.sg>
2025-05-22 11:07:49 +0800

    Completed BIDS validation: fixed NaN in event files, updated selected_subjects.tsv for 20 subjects

---
+++ get_all_runs.py
@@ -0,0 +1,13 @@
+from bids import BIDSLayout
+import os
+layout = BIDSLayout('/home/dingxuan/hu_project/ds005899', validate=False)
+selected_ids = pd.read_csv('/home/dingxuan/hu_project/selected_subjects.tsv', sep='\t')['participant_id'].tolist()
+for sub in selected_ids:
+    bold_files = layout.get(subject=sub, task='csst', suffix='bold', run=None, extension='.nii.gz')
+    event_files = layout.get(subject=sub, task='csst', suffix='events', run=None, extension='.tsv')
+    t1w_files = layout.get(subject=sub, suffix='T1w', extension='.nii')
+    for f in bold_files + event_files + t1w_files:
+        print(f"Getting {f.path}")
+        os.system(f"datalad get {f.path}")
+
+
